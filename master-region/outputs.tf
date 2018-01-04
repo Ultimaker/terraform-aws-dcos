6749b4aff60edd44ac76f2489b4e93fd5217d1cf
Yordi Pauptit <YP28@users.noreply.github.com>
2018-01-04 10:36:24 +0100

    add instance ip lists to output (#3)

--- master-region/outputs.tf
+++ master-region/outputs.tf
@@ -2,6 +2,10 @@ output "private-master-lb-dns" {
   value = "${aws_lb.master-prvt-lb.dns_name}"
 }
 
+output "bastion-ip" {
+  value = "${module.bootstrap-node.bastion-ip}"
+}
+
 output "bootstrap-ip" {
   value = "${module.bootstrap-node.bootstrap-ip}"
 }
@@ -16,4 +20,19 @@ output "number-of-masters" {
 
 output "exhibitor-bucket" {
   value = "${module.bootstrap-node.exhibitor-bucket}"
+}
+
+output "master-public-ip-list" {
+  value = ["${module.master-instances.master-public-ip-list}"]
+}
+
+output "public-slave-public-ip-list" {
+  value = ["${module.public-slave-instances.public-slave-public-ip-list}"]
+}
+
+output "private-slave-private-ip-list" {
+  value = [
+    "${module.private-slave-instances.private-slave-private-ip-list}",
+    "${module.private-gpu-slave-instances.private-slave-private-ip-list}",
+  ]
 }
